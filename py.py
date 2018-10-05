bee95b6415658f856e2ba5ef22bccaa534916364
interisland <43894177+interisland@users.noreply.github.com>
2018-10-05 23:46:46 +0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,18 @@
+a = input('')
+b = input('')
+c = input('')
+if a>b:
+	if a>c:
+		print(a)
+else:
+	if b>c:
+		print(b)
+	else:
+		print(c)
+
+a = 123123
+result = 0
+for element in a:
+	result = result + elemt
+print (result)
+
